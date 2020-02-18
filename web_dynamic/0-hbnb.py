69b8f8bd1a68885bb78473304cbe1db6d409b5ac
alejogonza <alejo.1996.2001@gmail.com>
2020-02-18 01:52:16 +0000

    [feature] add all

---
+++ web_dynamic/0-hbnb.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+ HTML Template
+"""
+from flask import Flask, render_template, url_for
+from models import storage
+from uuid import uuid4
+
+
+# flask setup
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+port = 5000
+host = '0.0.0.0'
+
+
+# begin flask page rendering
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    .close() SQLAlchemy Session
+    """
+    storage.close()
+
+
+@app.route('/0-hbnb')
+def hbnb_filters(the_id=None):
+    """
+    handles request
+    """
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+
+    return render_template('0-hbnb.html',
+                           states=states,
+                           amens=amens,
+                           places=places,
+                           users=users,
+                           cache_id=uuid4())
+    """
+    cache_id = new requirement
+    """
+
+if __name__ == "__main__":
+    """
+    Flask App
+    """
+    app.run(host=host, port=port)
